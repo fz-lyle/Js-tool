8b78d26b00750bcd2d31014eee6fed1f4f64c37a
朱昆鹏 <270750933@qq.com>
2018-06-29 09:18:55 +0800

    08.6.29

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ##### src 目录介绍
 - performance.js ：性能优化代码库
 - toole.js ：工具方法库
-- 
+- zq.js ：自己封装的jQuery库（简易版）
--- src/toole.js
+++ src/toole.js
@@ -15,6 +15,7 @@ function type(target) {
         '[object Number]' : 'number - object',
         '[object Boolean]' : 'boolean - object',
         '[object String]' : 'string - object',
+        '[object Date]' : 'date',        
     }
     var ret = typeof(target)
 
@@ -30,18 +31,41 @@ function type(target) {
     }
 
 }
-this.type = type;
 
 
 /**
  * 随机生成随机数
+ * { randomNum(随机数生成的范围整数) }
  */
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
+
+
+
+
 
 
 /**
  * 继承方法
  * {}
  */
+
+ // 分为两种
+ // 1. 一种是继承构造函数，然后所有通过new 出来的对象，就带有 多代构造函数的属性和方法
+ // 2. 一种是任意对象继承，这个是混入的思想，就是将任意多个的对象继承到一个对象里面
+
+
 
  
  /**
@@ -63,6 +87,22 @@ Array.prototype.unique = function () {
 }
 
 
+/**
+ * 对象拷贝的方法
+ */
 
+
+ // 利用挂载函数，把所有的方法都挂载到全局上面  以后肯定要做一个 挂载到全局上面的方法的，只是现在还是不太清楚
+function guazai () {
+    window.type = type;
+    window.randomNum = randomNum;
+}
+
+// 初始化函数
+function init() {
+    guazai()
+}
+
+init()
 
 }())
